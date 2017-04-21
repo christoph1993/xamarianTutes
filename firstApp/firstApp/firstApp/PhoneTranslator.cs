9bfb6f6f631b2590fc00a8474e5ca9b4362b8a73
Christoph Brinkmann <christoph.brinkmann93@gmail.com>
2017-04-21 10:19:21 +1000

    First Xamarian applications based from tutorials

---
+++ firstApp/firstApp/firstApp/PhoneTranslator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    class PhoneTranslator
+    {
+        static readonly string[] digits =
+        {
+            "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"
+        };
+
+        public static string ToNumber(string raw)
+        {
+            if (string.IsNullOrWhiteSpace(raw)) return null;
+
+            raw = raw.ToUpperInvariant();
+
+            var number = new StringBuilder();
+            foreach(var no in raw)
+            {
+                if ("-0123456789".Contains(no.ToString())) number.Append(no);
+                else
+                {
+                    var translated = TranslateToNumber(no);
+                    if (translated != null) number.Append(translated);
+                    else return null;
+                }
+            }
+
+            return number.ToString();
+        }
+
+        static int? TranslateToNumber(char c)
+        {
+            for(int i = 0; i < digits.Length; i++)
+            {
+                if (digits[i].Contains(c.ToString())) return 2 + i;
+            }
+            return null;
+        }
+    }
+}
---
+++ multipage-application/multipage-application/multipage_application/helloPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace multipage_application
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class helloPage : ContentPage
+    {
+        public helloPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void SayHelloButton_OnClicked(object sender, EventArgs e)
+        {
+            var name = NameEntry.Text;
+            await DisplayAlert("Greeting", $"Hello {name}!!","G'day");
+        }
+    }
+}
